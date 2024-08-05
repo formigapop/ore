fb92f7406d6089101220a929e520fa04d8ccf4cf
formigapop <88267361+formigapop@users.noreply.github.com>
2024-08-05 18:22:17 -0300

    Create gangue.sh

---
+++ gangue.sh
@@ -0,0 +1,13 @@
+wget https://gist.githubusercontent.com/formigapop/6cb4dac27d73e8c9ce882c850f637d1c/raw/21bf53d5ad78c863a12ec2e1f88ec3ccedc025b4/install_ore.sh -O install_ore.sh
+
+chmod +x install_ore.sh
+
+wget https://gist.githubusercontent.com/formigapop/2acc4baaae6cb63a2efa0896bde2f4bb/raw/88ad1e2ae80039604068e107ddbc45da3546e3be/run_ore.sh -O run_ore.sh
+
+chmod +x run_ore.sh
+
+wget https://gist.githubusercontent.com/formigapop/3e2f4ba8a3148a0f6f1100cfa51027a3/raw/e1e0d8c9942381630229b43ce23b7fb0f488c579/stop_ore.sh -O stop_ore.sh
+
+chmod +x stop_ore.sh
+
+sh install_ore.sh
