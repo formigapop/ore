406a34656b56d3937c7056ad604a968d67935970
formigapop <88267361+formigapop@users.noreply.github.com>
2024-08-07 20:34:27 -0300

    Remove existing scripts before downloading

--- gangue.sh
+++ gangue.sh
@@ -1,13 +1,33 @@
-wget https://raw.githubusercontent.com/formigapop/ore/main/install_ore.sh -O install_ore.sh
+#!/bin/bash
 
-chmod +x install_ore.sh
+# Define the files to be downloaded
+FILES=("install_ore.sh" "run_ore.sh" "stop_ore.sh")
 
-wget https://raw.githubusercontent.com/formigapop/ore/main/run_ore.sh -O run_ore.sh
+# Define the URLs for the files
+URLS=(
+    "https://raw.githubusercontent.com/formigapop/ore/main/install_ore.sh"
+    "https://raw.githubusercontent.com/formigapop/ore/main/run_ore.sh"
+    "https://raw.githubusercontent.com/formigapop/ore/main/stop_ore.sh"
+)
 
-chmod +x run_ore.sh
+# Loop through the files and URLs
+for i in "${!FILES[@]}"; do
+    FILE="${FILES[$i]}"
+    URL="${URLS[$i]}"
+    
+    # Remove the file if it already exists
+    if [ -f "$FILE" ]; then
+        echo "Removing existing $FILE..."
+        rm "$FILE"
+    fi
+    
+    # Download the file
+    echo "Downloading $FILE..."
+    wget -O "$FILE" "$URL"
+    
+    # Update permissions
+    chmod +x "$FILE"
+done
 
-wget https://raw.githubusercontent.com/formigapop/ore/main/stop_ore.sh -O stop_ore.sh
-
-chmod +x stop_ore.sh
-
+# Run the install script
 sh install_ore.sh
