484ce6dd303e0a8fb823e0eb3740508d4b084df4
formigapop <88267361+formigapop@users.noreply.github.com>
2024-08-11 14:36:06 -0300

    ore cli 2.3

--- run_ore.sh
+++ run_ore.sh
@@ -6,6 +6,7 @@
 RPC_URL="https://api.mainnet-beta.solana.com"
 PRIORITY_FEE=4000
 CORES=""
+JITO=""
 
 # Parse arguments
 while [[ $# -gt 0 ]]; do
@@ -18,12 +19,16 @@ while [[ $# -gt 0 ]]; do
             PRIORITY_FEE=$2
             shift 2
             ;;
+        --jito)
+            JITO="--jito"
+            shift 2
+            ;;
         --cores)
             CORES="--cores $2"
             shift 2
             ;;
         *)
-            echo "Usage: $0 [--rpc <rpc_url>] [--priority-fee <fee>] [--cores <num_cores>]"
+            echo "Usage: $0 [--rpc <rpc_url>] [--priority-fee <fee>] [--cores <num_cores>] [--jito]"
             exit 1
             ;;
     esac
@@ -38,7 +43,7 @@ LOG_FILE="ore_mainnet.log"
 CLAIM_LOG_FILE="ore_mainnet_claim.log"
 
 # Start the ore process in the background
-nohup $ORE_EXEC --rpc $RPC_URL --keypair $KEYPAIR_PATH mine --priority-fee $PRIORITY_FEE --buffer-time 2 $CORES > $LOG_FILE 2>&1 &
+nohup $ORE_EXEC --rpc $RPC_URL --keypair $KEYPAIR_PATH mine --priority-fee $PRIORITY_FEE --buffer-time 2 $CORES $JITO > $LOG_FILE 2>&1 &
 
 # Get the PID of the ore process
 echo $! > ore.pid
